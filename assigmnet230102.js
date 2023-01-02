f16aebc20b33351e35504057122c7e5acd6afc69
jindol <parkmoc21@gmail.com>
2023-01-03 01:44:19 +0900

    230103 01:44 2일 미션 중 최대 팩토리얽값 구하기 문제 실패 그 외 문제는 구현 성공

--- assigmnet230102.js
+++ assigmnet230102.js
@@ -1,6 +1,6 @@
 // 가장 큰 수 찾기
 // https://school.programmers.co.kr/learn/courses/30/lessons/120899
-
+/*
 var arr = [5, 9, 2, 16, 1, 4];
 
 function findMaxNum(array) {
@@ -16,9 +16,34 @@ function findMaxNum(array) {
 }
 
 console.log(findMaxNum(arr));
-
+*/
 
 // 자리수 더하기
 // https://school.programmers.co.kr/learn/courses/30/lessons/120906
+// 10으로 나누는 원리는 이해했지만 구현을 어떻게 해야할지 몰라 인터넷을 참고함: https://pika-chu.tistory.com/287
 
-console.log(1234/1)
+/*
+let n= 930211;
+
+function answer() {
+    let sum = 0;
+    while (n > 0) {
+        sum += Math.floor(n % 10);
+        n = Math.floor(n / 10);
+    }
+    return sum;
+}
+*/
+
+// 팩토리얼
+// https://school.programmers.co.kr/learn/courses/30/lessons/120848
+
+let n = 7;
+let count = 1;
+
+while (count <= 10) {
+    n = Math.floor(n/count);
+    console.log(n);
+    count++;
+            
+}
